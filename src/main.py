a7f09c551ef8ad5a743239f8a196f57e4809727f
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-04-17 11:05:53 -0600

    screen resolution

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 import pyglet
 from game.menu import Menu
 
-window = pyglet.window.Window(800, 600, caption='Geometry Dash Clone')
+platform = pyglet.canvas.get_display().get_default_screen()
+screen_width = platform.width
+screen_height = platform.height
+
+window_width = int(screen_width * 0.8)
+window_height = int(screen_height * 0.8)
+
+min_window_width = 1280
+min_window_height = 720
+
+if window_width < min_window_width:
+    window_width = min_window_width
+if window_height < min_window_height:
+    window_height = min_window_height
+
+window = pyglet.window.Window(width=window_width, height=window_height, resizable=True, caption='Geometry Dash Clone')
 menu = Menu()
 
 @window.event
